d8938160661582c2db975d3a70af3e76e8b3730f
Ibrahim <iabdelgawaad@gmail.com>
2021-07-15 23:15:05 +0200

    - Added currency converter functionality

--- app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
+++ app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
@@ -1,11 +1,13 @@
 package com.example.codechallenge.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import com.example.codechallenge.R
+import kotlinx.android.synthetic.main.fragment_currency_converter.*
 
 class CurrencyConverterFragment : Fragment() {
     private var currencyName: String? = null
@@ -21,12 +23,28 @@ class CurrencyConverterFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_currency_converter, container, false)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        target_currency_name?.text = currencyName
+        target_currency_edit?.setText(currencyValue)
+
+        base_currency_edit?.doOnTextChanged { inputText, _, _, _ ->
+            inputText?.let {
+                it?.isNotEmpty()?.let { notEmpty ->
+                    if (notEmpty) {
+                        val input = inputText?.toString()?.toDouble()
+                        currencyValue?.toDouble()?.let {
+                            target_currency_edit?.setText(input?.times(it)?.toString())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
