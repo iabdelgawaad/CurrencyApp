900d4fa305e04dd532b6b8886ea5b08a2a8b9e9e
Ibrahim <iabdelgawaad@gmail.com>
2021-07-15 18:23:18 +0200

    - added loading indicator

--- app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
+++ app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
@@ -39,15 +39,6 @@ class CurrencyConverterFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CurrencyConverterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CurrencyConverterFragment().apply {
@@ -56,5 +47,9 @@ class CurrencyConverterFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/com/example/codechallenge/view/MainActivity.kt
+++ app/src/main/java/com/example/codechallenge/view/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.codechallenge.view
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.codechallenge.R
 import com.example.codechallenge.data.model.LatestExchangeRatesResponseModel
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
@@ -19,12 +21,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+        showLoading()
         viewModel?.getRates()?.enqueue(object : Callback,
             retrofit2.Callback<LatestExchangeRatesResponseModel?> {
             override fun onResponse(
                 call: Call<LatestExchangeRatesResponseModel?>,
                 baseResponse: Response<LatestExchangeRatesResponseModel?>
             ) {
+                hideLoading()
                 Log.d("result", baseResponse?.message())
                 baseResponse?.isSuccessful?.let {
                     baseResponse?.body()?.success?.let { isSuccess ->
@@ -46,9 +50,19 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(
-                baseResponse: Call<LatestExchangeRatesResponseModel?>, t: Throwable) {
+                baseResponse: Call<LatestExchangeRatesResponseModel?>, t: Throwable
+            ) {
+                hideLoading()
                 t?.message?.let { Log.d("result", it) }
             }
         })
+    }
+
+    fun showLoading() {
+        progress_bar?.visibility = View.VISIBLE
+    }
+
+    fun hideLoading() {
+        progress_bar?.visibility = View.GONE
     }
 }
