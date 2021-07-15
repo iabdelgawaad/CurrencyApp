96ee97609389068244a004cd3071fe5bab3c783a
Ibrahim <iabdelgawaad@gmail.com>
2021-07-15 15:38:38 +0200

    fixed crash while showing dialog

--- app/src/main/java/com/example/codechallenge/view/MainActivity.kt
+++ app/src/main/java/com/example/codechallenge/view/MainActivity.kt
@@ -4,12 +4,13 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.codechallenge.R
 import com.example.codechallenge.data.model.LatestExchangeRatesResponseModel
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: ViewModel
@@ -17,31 +18,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
-
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
         viewModel?.getRates()?.enqueue(object : Callback,
             retrofit2.Callback<LatestExchangeRatesResponseModel?> {
             override fun onResponse(
                 call: Call<LatestExchangeRatesResponseModel?>,
-                baseResponse: Response<LatestExchangeRatesResponseModel?>) {
+                baseResponse: Response<LatestExchangeRatesResponseModel?>
+            ) {
                 Log.d("result", baseResponse?.message())
                 baseResponse?.isSuccessful?.let {
                     baseResponse?.body()?.success?.let { isSuccess ->
                         if (isSuccess) {
 
                         } else {
-                            AlertDialog.Builder(this@MainActivity)
-                                .setTitle(getString(R.string.error_alert_dialog_title))
-                                .setMessage(baseResponse?.body()?.error?.info)
-                                .setPositiveButton(getString(R.string.ok_text), null)
-                                .show()
+                            this@MainActivity?.let {
+                                val dialog = AlertDialog.Builder(it)
+                                    .setTitle(getString(R.string.error_alert_dialog_title))
+                                    .setMessage(baseResponse?.body()?.error?.info)
+                                    .setPositiveButton(getString(R.string.ok_text), null)
+                                if (!(this@MainActivity).isFinishing) {
+                                    dialog?.show()
+                                }
+                            }
                         }
                     }
                 }
             }
 
             override fun onFailure(
-                baseResponse: Call<LatestExchangeRatesResponseModel?>, t: Throwable) {
+                baseResponse: Call<LatestExchangeRatesResponseModel?>, t: Throwable
+            ) {
                 t?.message?.let { Log.d("result", it) }
             }
         })
