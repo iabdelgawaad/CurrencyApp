4f4567d260a00d0fcb50b983b41d164055bb6df6
Ibrahim <iabdelgawaad@gmail.com>
2021-07-15 22:54:47 +0200

    - added currency converter edittext

--- app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
+++ app/src/main/java/com/example/codechallenge/view/CurrencyConverterFragment.kt
@@ -7,47 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.codechallenge.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CurrencyConverterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CurrencyConverterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var currencyName: String? = null
+    private var currencyValue: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            currencyName = it.getString("SELECTED_CURRENCY_NAME")
+            currencyValue = it.getString("SELECTED_CURRENCY_VALUE")
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_currency_converter, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CurrencyConverterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
